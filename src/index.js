5a3c7bec1a0700121fbedc61255ea1fd97cee65a
OlenaPustova <i.am.elena.p@gmail.com>
2024-06-07 20:52:45 +0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./scss/main.scss";
+import "./js/event";
+import "./js/favorite-city";
+import "./js/quote";
+import "./js/bg-api";
+import "./js/home-weather";
+import "./js/home-day";
+import "./js/five-days";
+import "./js/chart";
+import "./js/marcup";
+import "./js/geolocation";
